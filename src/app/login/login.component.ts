43701f8410dd6953059c7e7cb97b0eb5fa113ca3
zeba27 <zebasotota71318@gmail.com>
2024-03-25 01:44:19 +0600

    Login page created

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,12 +2,13 @@ import {Component} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
