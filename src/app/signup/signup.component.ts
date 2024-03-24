c80b6cce1a3f5355cbd4bcf87e1650cc6b2dd58b
zeba27 <zebasotota71318@gmail.com>
2024-03-25 02:28:45 +0600

    SignUp page created

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
